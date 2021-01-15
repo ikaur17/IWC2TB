746700e50e7b06dad78e788b916cc31b5296491c
IKaur <ikaur1985@gmail.com>
2021-01-15 10:06:07 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="IWC2TB",
+    version="0.0.1",
+    author="Inderpreet Kaur",
+    description="Scripts to read and analyse ARTS simulations (Radar2Tb) ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/SEE-MOF/IWC2TB",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU Affero",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[ "numpy", "scipy"],
+    python_requires=">=3.6",
+    include_package_data=True,
+    package_data={
+        '': ['*.ini']
+    }
+)
+
