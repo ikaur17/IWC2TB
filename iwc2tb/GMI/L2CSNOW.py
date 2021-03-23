005323a1f4d9234c523d7319c6f37a5e98542602
IKaur <ikaur1985@gmail.com>
2021-03-23 14:16:24 +0100

    class to read L2CSP

--- iwc2tb/GMI/L2CSNOW.py
+++ iwc2tb/GMI/L2CSNOW.py
@@ -12,13 +12,17 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from era2dardar.utils.Z2dbZ import Z2dbZ
-from pansat.products.satellite.cloud_sat import l2b_geoprof, l2c_snow
+from pansat.products.satellite.cloud_sat import l2b_geoprof
 from matplotlib.colors import Normalize
 import matplotlib.colors as colors
 from matplotlib import cm
 from pansat.formats.hdf4 import HDF4File
 from scipy import interpolate
+import glob
+from iwc2tb.common.hist2d import hist2d
 
+import xarray
+#%%
 class SNOW2C():
     
     def __init__(self, filename):
@@ -29,8 +33,22 @@ class SNOW2C():
         
         datasets_dic = self.file.datasets()
         
-        print (datasets_dic.keys() )
+        print (datasets_dic.keys())
+        
+        self.SWC        = self.__getitem__("snow_water_content")
+        self.R          = self.__getitem__("snowfall_rate")
+        self.logN0      = self.__getitem__("log_N0")
+        self.loglambda  = self.__getitem__("log_lambda")   
+        self.height     = self.__getitem__("Height")
+        
+        
+    def __getitem__(self, key):
+        
+        sds_obj = self.file.select(key) # select sds
+        data = sds_obj.get() 
+        return data
 
+#%%
 if __name__ == "__main__":        
 
     path = "/home/inderpreet/Downloads" 
@@ -40,76 +58,94 @@ if __name__ == "__main__":
     
     path1 = "/home/inderpreet/Dendrite/Projects/IWP/GMI/2C-SNOW/"
     file1 = "2006355074548_03451_CS_2C-SNOW-PROFILE_GRANULE_P1_R05_E02_F00.hdf"
-    file1 = "2010027071721_19950_CS_2C-SNOW-PROFILE_GRANULE_P1_R05_E03_F00.hdf"
+    file2 = "2010027071721_19950_CS_2C-SNOW-PROFILE_GRANULE_P1_R05_E03_F00.hdf"
     
     
-    file2 = "2010027071721_19950_CS_2C-PRECIP-COLUMN_GRANULE_P1_R05_E03_F00.hdf"
+    #file2 = "2010027071721_19950_CS_2C-PRECIP-COLUMN_GRANULE_P1_R05_E03_F00.hdf"
     
     
     filename = os.path.join(path, file)
     filename1 = os.path.join(path1, file1)   
     filename2 = os.path.join(path, file2)
     
-    s2c = SNOW2C(filename1)
-    sds_obj = s2c.file.select("snow_water_content") # select sds
-    swc = sds_obj.get() 
+    filenames = glob.glob("/home/inderpreet/Dendrite/SatData/CloudSat/2C-SNOW.R05/*.hdf")
+    #dataset = l2b_geoprof.open(filename2)
     
-    dataset = l2b_geoprof.open(filename)
+#%%
+    SWC    = []
+    R      = []
+    logN0  = []
+    logL   = []
+    for filename in filenames[:1]:
+        s2c = SNOW2C(filename)        
+        SWC.append(s2c.SWC)
+        R.append(s2c.R)
+        logN0.append(s2c.logN0)
+        logL.append(s2c.loglambda) 
+        
+    SWC   = np.concatenate(SWC, axis = 0)    
+    logN0 = np.concatenate(logN0, axis = 0)
+    logL  = np.concatenate(logL, axis = 0)
+    R     = np.concatenate(R, axis = 0)
     
-    start = 10000
-    end = 25000
-    stride = 100
-    z = dataset["radar_reflectivity"]/100
-    z = np.where(z < -30, np.nan, z)
-    lats = dataset["latitude"]
-    bins = dataset["bins"]
-    lons = dataset["longitude"]
-    height = dataset["height"]
+    SWC     = np.where(SWC < -900, np.nan, SWC)
+    logN0   = np.where(logN0 < -900, np.nan, logN0)
+    logL    = np.where(logL< -900, np.nan, logL)
+    R       = np.where(R< -900, np.nan, R)
     
-    #%% interpolate to common height
+      
+    N0 = 10 ** logN0
+    L  = 10 ** logL
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize = [20, 8])
+    fig.tight_layout(pad=3.0)
     
-    h   = np.arange(0, 10000, 250)  
-    grid_z = np.zeros([lats.shape[0], h.shape[0]])
-    for i in range(lats.shape[0]): 
-        f               = (interpolate.interp1d(height[i, :], z[i, :],
-                                           fill_value = "extrapolate"))
-        grid_z[i, :]          = f(h) 
-    
-    #%%
-    
-    
-    
-    mask = (lats >= 35.5) & (lats <= 36)
-    mask1  = (lons < 100) & (lons > 60)
-    mask = np.logical_and(mask, mask1)
-    
-    fig, ax = plt.subplots(1, 1, figsize=(20, 6))
-    cs = ax.pcolormesh(lats[mask], h/1000, grid_z[mask, :].T, norm=Normalize(-30, 40), cmap = cm.rainbow)
-    fig.colorbar(cs, label="dbZe")
-    ax.set (ylim=(0, 10))
-    #plt.gca().invert_yaxis()
-    plt.xlabel(r"Latitude [deg]")
-    plt.ylabel("Height [km]");
-    plt.title("L2B-GEOPROF", loc="left")
-    fig.savefig("cloudsat_radar_reflectivity.pdf", bbox_inches = "tight")
-    
-    swc1 = np.where(swc < 0, np.nan, swc)
-    # plt.figure(figsize=(10, 5))
-    # plt.pcolormesh(lats[mask], height[1, :]/1000, swc1[mask, :].T, norm=Normalize(0, 0.5), cmap = cm.rainbow)
-    # plt.colorbar(label="Snow water content [g/m3] ")
-    # #plt.gca().invert_yaxis()
-    # plt.xlabel(r"Latitude [$^\circ\ N$]")
-    # ax.set (ylim=(0, 10))
-    # plt.ylabel("Height[km]");
-    # plt.title("L2C-SNOW", loc="left")
+    ax1.scatter(R, N0, alpha = 0.3)
+    ax1.set_yscale("log")    
+    ax1.set_xlabel(r"R [mm h$^{-1}$]")
+    ax1.set_ylabel(r"N0 [m$^{-3}$ mm$^{-1}$]")
+    ax1.text(0.3, 0.8, r'N0 = 3800 R$^{-0.87}$ ', transform=ax1.transAxes)
     
     
-    fig, ax = plt.subplots(1, 1, figsize=(20, 6))
-    cs = ax.pcolormesh(lats[mask], height[1, :]/1000, swc1[mask, :].T /1000, norm=Normalize(0, 0.002), cmap = cm.rainbow)
-    fig.colorbar(cs, label="SWC [kg/m3]")
-    ax.set (ylim=(0, 10))
-    #plt.gca().invert_yaxis()
-    plt.xlabel(r"Latitude [deg]")
-    plt.ylabel("Height [km]");
-    plt.title("L2C-SNOW", loc="left")
-    #fig.savefig("cloudsat_radar_reflectivity.pdf", bbox_inches = "tight"
+    ax2.scatter(R, L, alpha = 0.3)
+    ax2.set_yscale("log")
+    ax2.set_xlabel(r"R [mm h$^{-1}$]")
+    ax2.set_ylabel(r"$\Lambda$ [mm$^{-1}$] ")
+    ax2.text(0.3, 0.8, r'$\Lambda$ = 2.55 R$^{-0.48}$ ', transform=ax2.transAxes)
+
+    R1 = np.sort(R.ravel())
+    y1 = 3800 * R1 **(-0.87)    
+    ax1.plot(R1, y1.ravel(), 'k')
+    
+    y2 = 2.55 *  R1**(-0.48)
+    ax2.plot(R1, y2.ravel(), 'k')
+    
+    
+    ax3.scatter(N0, L, alpha = 0.3)
+    ax3.set_yscale("log")
+    ax3.set_yscale("log")
+    ax3.set_xlabel(r"N0 [m$^{-3}$ mm$^{-1}$]]")
+    ax3.set_ylabel(r"$\Lambda$ [mm$^{-1}$] ")
+    ax3.text(0.3, 0.8, r'$\Lambda$ = 2.55 R$^{-0.48}$ ', transform=ax2.transAxes)
+    
+    fig.savefig("size_distribution_2CSP.png", bbox_inches = "tight")
+    
+#%%
+    height = s2c.height
+    bins   = np.arange(0, 6000, 1)
+    Range  = np.arange(0, N0.shape[1], 1)
+    XN0 = xarray.DataArray(N0[3000:9000, :], coords = [bins, Range], dims = ['bins', 'range'], name = 'N0')  
+    XL  = xarray.DataArray(L[3000:9000, :], coords = [bins, Range], dims = ['bins', 'range'], name = 'lambda') 
+    XH  = xarray.DataArray(height[3000:9000, :], coords = [bins, Range], dims = ['bins', 'range'], name = 'height') 
+    XR   = xarray.DataArray(R[3000:9000, :], coords = [bins, Range], dims = ['bins', 'range'], name = 'snowfall_rate') 
+ 
+    A = xarray.merge([XN0, XL, XH, XR])
+    A.to_netcdf('L2CSNOW.nc', 'w')
+    
+#%%
+    
+    
+    
+    
+    
+    
